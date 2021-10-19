3e7ef1972d933b0504b79373c536377243315244
SimJoon <wesley0940@gmail.com>
2021-10-19 22:31:37 +0900

    20211019

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 #![allow(non_snake_case)]
 fn main() {
-    println!("Hello, world!");
+    // addition
+    let sum = 5 + 10;
+    println!("{}",sum);
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("{}",difference);
+    // multiplication
+    let product = 4 * 30;
+    println!("{}",product);
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("{}",quotient);
+    // remainder
+    let remainder = 43  5;
+    println!("{}",remainder);
 }
