ac2284d0113ea31ecbc6364fb88ff80bf0df2b71
SimJoon <wesley0940@gmail.com>
2021-10-20 01:05:31 +0900

    loop

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,7 @@
 #![allow(non_snake_case)]
 
 fn main() {
-    let condition = true;
-    let number = if condition {
-        5
-    } else {
-        6
-    };
-
-    println!("The value of number is: {}", number);
+    loop {
+        println!("again!");
+    }
 }
