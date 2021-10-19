e3b72747b7352f2c97350a49d37baad64a048a71
SimJoon <wesley0940@gmail.com>
2021-10-20 00:55:35 +0900

    function3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-fn main() {
-    another_function(5, 6);
-}
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
+#![allow(non_snake_case)]
+
+fn main() {
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
     println!("The value of y is: {}", y);
 }
