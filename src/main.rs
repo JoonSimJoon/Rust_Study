7a6a099a99140e6d755ebc93454eda2a00782a39
SimJoon <wesley0940@gmail.com>
2021-10-21 23:57:06 +0900

    reference 1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 
 #![allow(non_snake_case)]
 fn main() {
-    let s = String::from("hello");  
+    let s1 = String::from("hello");
 
-    takes_ownership(s);     
-        
-    let x = 5;                     
+    let len = calculate_length(&s1);
 
-    makes_copy(x);                  
-    println!("{}",x);
-} 
+    println!("The length of '{}' is {}.", s1, len);
+}
 
-fn takes_ownership(some_string: String) { 
-    println!("{}", some_string);
-} 
-
-fn makes_copy(some_integer: i32) { 
-    println!("{}", some_integer);
-} 
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
