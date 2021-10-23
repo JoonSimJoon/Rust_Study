fed201937f4b462d53e41154d066adbbc074458f
SimJoon <wesley0940@gmail.com>
2021-10-23 17:07:18 +0900

    struct1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+#![allow(non_snake_case)]
 #![allow(unused)]
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    let arr = [1,2,3,4];
-    let brr = &arr[0..2];
-    println!("{}",brr[0]);
+    let mut arr = User{
+        username: String::from("adasda"),
+        email:String::from("wesl@come!"),
+        sign_in_count: 13,
+        active: true,
+    };
+    let brr = arr.username;
+    println!("{}",brr);
 }
